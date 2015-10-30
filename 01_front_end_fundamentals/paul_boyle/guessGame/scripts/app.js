b386097059aab5522da04e9d088f0115c7cdf0ba
Paul Boyle <pboyle17@gmail.com>
2015-10-30 12:52:25 -0500

    lunch time update

--- 01_front_end_fundamentals/paul_boyle/guessGame/scripts/app.js
+++ 01_front_end_fundamentals/paul_boyle/guessGame/scripts/app.js
@@ -1,39 +1,43 @@
-window.onload= function(){
-  alert('window loaded!');
+window.onload=function() {
 
-  var askGuess= function () {
-    var number=parseInt(10*(Math.random()));
-    var ans;
-    var win;
-    console.log('the number is :'+number);
+  var getAns = function () {
+    return parseInt(10*(Math.random()));
+  }; //end of getAns function
 
-    var guess=function(){
-      ans=parseInt(prompt('Guess a number between 1-10'));
-      console.log('guess:'+ans);
-      return ans;
-    }
-    console.log(guess);
-    guess();
-    for (var i = 0; i < 2; i++) {
-        if(ans>number){
-          console.log('A little lower');
-          return guess();
-        }
-        else if (ans<number){
-          console.log('A little higher');
-          return guess();
-        }
-        else if(ans==number){
-          win=true;
-          return console.log('Congrats you got it right!');
-        } // end of if else if esle statments
-    } // end of for loop
-    if(!win){
-      console.log('sorry you lost');
-    }//win conditoin
-    else {
-      console.log('you won great job!');
-    }
-  } // end of askGuess
-  askGuess();
-} //end of window.onload()
+  var getGuess=function() {
+    return parseInt(prompt('pick a number between 1 and 10'));
+  }; //end of getGuess function
+
+  var playGame=function(){
+    while (win == false) {
+      if(guess==ans){
+        console.log('you win!');
+        win=true;
+      }
+      else if (guess>ans){
+        console.log('lower');
+        guess=getGuess();
+        console.log('new guess: '+guess+' '+typeof(guess));
+      }
+      else {
+        console.log('higher');
+        guess=getGuess();
+        console.log('new guess: '+guess+' '+typeof(guess));
+      }
+    }// end of while loop
+
+  }//end of playGame function
+
+/////////////////////////////////////////////////////////////////
+
+  var win=false; // setting a condition for the game to play.
+  var ans=getAns();
+  var guess=getGuess();
+
+
+  //console.log('answer: ' + ans);
+  console.log('guess: '+ guess);
+
+  playGame();//play the game
+
+}//end of window.onload
