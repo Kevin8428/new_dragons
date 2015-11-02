7db53869eebda23be78ac1a0a162a9ef5ad217b8
Sam Groesch <samgroesch@gmail.com>
2015-11-02 10:16:33 -0600

    Halfway through block 1

---
+++ 02_oojs_jquery/sam_groesch/objects_again.js
@@ -0,0 +1,32 @@
+var james = {
+  sayMyName: function() {
+    return 'I am James';
+  }
+
+};
+
+var err = {
+  sayMyName: function() {
+    return 'I am Error';
+  },
+  makeFunOfGreenClothes: function() {
+    return 'Your clothes look silly, little elf man'
+  }
+
+};
+
+var sam = {
+  sayMyName: function() {
+    return 'I am Sam'
+  },
+  eyeColor: 'Brown',
+    hairColor: 'Brown',
+    height: '5\'9\"',
+    weight: '170 lbs',
+    hometown: 'Glen Ellyn, IL',
+    college: 'Iowa State University',
+    degree: 'Economics',
+    shoeSize: '10',
+    favoriteSport: 'basketball',
+    numberOfSiblings: '2'
+}
