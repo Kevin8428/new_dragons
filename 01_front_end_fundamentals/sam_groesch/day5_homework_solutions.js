cab77a16f7909be4f1b71c42a132d0b91cac7a6e
Sam Groesch <samgroesch@gmail.com>
2015-11-02 09:37:26 -0600

    Finished going over weekend homework

--- 01_front_end_fundamentals/sam_groesch/day5_homework_solutions.js
+++ 01_front_end_fundamentals/sam_groesch/day5_homework_solutions.js
@@ -16,4 +16,43 @@ function transmogrifier(x, y, z) {
   return Math.pow((x*z), z);
 }
 
-// 3. 
+// 3. toonify
+
+function toonify(accent, sentence) {
+  if (accent == 'daffy'){
+    return sentence.replace(/s/g, 'th'); //Learn syntax for replace
+  } else if (accent = 'elmer') {
+    return sentence.replace(/r/g, 'w');
+  } else {
+    return sentence;
+  }
+}
+
+// 4. wordReverse
+
+function wordReverse (word) {
+  var tempArray = word.split('');
+  console.log(tempArray);
+  var reversed = tempArray.reverse();
+  console.log(reversed)
+  return reversed.join('');
+}
+
+// 5. letterReverse
+
+function letterReverse(someStuff) {
+  var tempArray = someStuff.split('');
+  var finalSentence;
+
+  for (var inc in tempArray) {
+    var word = tempArray[inc];
+    console.log(word);
+    var splitWord = word.split('');
+    splitWord.reverse();
+    word = splitWord.join('');
+    console.log(word);
+    finalSentence = finalSentence + ' ' + word;
+    console.log(finalSentence);
+  }
+  return finalSentence;
+}
