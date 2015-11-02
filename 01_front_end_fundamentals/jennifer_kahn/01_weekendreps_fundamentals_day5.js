28ca6245d8dc4438fbb34ccb67747be839ec05ac
Jen Kahn <jenniferhkahn@gmail.com>
2015-11-01 23:12:01 -0600

    weekendreps

--- 01_front_end_fundamentals/jennifer_kahn/01_weekendreps_fundamentals_day5.js
+++ 01_front_end_fundamentals/jennifer_kahn/01_weekendreps_fundamentals_day5.js
@@ -22,19 +22,45 @@ return Math.pow(t, num3);
 transmogrifier(5, 4, 3); ========> 8000
 transmogrifier(13, 12, 5) =========> 92389579776
 transmogrifier(42, 13, 7) ===========> 14466001271480793000
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
+
+
+
+function toonify(accent, sentence){
+
+
+  var str = "daffy, so you smell like sausage";
+  var newStr = str.replace(/so/i, 'tho');
+  var newStr = str.replace(/smell/i, 'thmell');
+  var newStr = str.replace(/sausage/i, 'thauthage');
+  console.log(newStr)
+}
+
+
+var wordReverse = function(string){
+    arr= string.split(' ');
+    arr.reverse();
+    var reversing= ' ';
+    for (word in arr){
+    reversing +=arr[word] + ' ';
+            }
+    return reversing;
+}
+console.log(wordReverse("now I know what a TV dinner feels like"));
+
+========> "like feels dinner TV a what know I now"
+
+
+
+//repMaster("Never give your heart to a blockhead", wordReverse) # =>
+//"blockhead a to heart your give never proves that I am the rep MASTER!"
+//repMaster("I finished this practice", toUpperCase);
+//"I FINISHED THIS PRACTICE proves that I am the rep MASTER!"
+
+
+function(user,myFunction){
+var repMaster
+return myFunction(user);
+}
+
+console.log(repMaster('never give your heart to a blockhead.wordReverse));
