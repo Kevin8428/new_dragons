c441675dc4319f930820905feb2e7f06fa4a383f
Jen Kahn <jenniferhkahn@gmail.com>
2015-11-01 23:23:47 -0600

    i made changes

--- 01_front_end_fundamentals/jennifer_kahn/01_weekendreps_fundamentals_day5.js
+++ 01_front_end_fundamentals/jennifer_kahn/01_weekendreps_fundamentals_day5.js
@@ -13,7 +13,6 @@ lengths(flowers)
 
 
 
-
 function transmogrifier(num1, num2, num3){
 var t = (num1 * num2);
 return Math.pow(t, num3);
@@ -23,7 +22,6 @@ transmogrifier(5, 4, 3); ========> 8000
 transmogrifier(13, 12, 5) =========> 92389579776
 transmogrifier(42, 13, 7) ===========> 14466001271480793000
 }
-
 
 
 function toonify(accent, sentence){
