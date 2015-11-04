1046f2676c2d8b31977aa8842c7841dda5ae7d62
Nick Espinosa <nick@espi.uno>
2015-11-04 16:39:19 -0600

    update

--- 02_oojs_jquery/nick_espinosa/morningloops/scripts/app.js
+++ 02_oojs_jquery/nick_espinosa/morningloops/scripts/app.js
@@ -1,15 +1,28 @@
-var fiveFoods = ['pizza', 'cookies', 'brownies', 'rice krispy bars','lucky charms'];
-for (var food in fiveFoods) {
-  console.log("I like " + fiveFoods[food]);
-}
-
 var nick = {
   name: 'nick',
   hobby: 'working out',
   interests: 'computers',
   movies: ['idiocracy', 'dumb and dumber', 'bourne identity', 'bourne supremecy']
 }
+var foods = ['pizza', 'cookies', 'brownies', 'rice krispy bars','lucky charms'];
 
-nick.sports = function() {
-  return this.sports; 
+
+window.onload = function() {
+var btn = document.getElementById('btn');
+
+for (var i = 0; i < foods.length; i++) {
+  console.log(foods[i] + ' is a great food');
 }
+
+nick.print = function(arr) {
+  for (var i = 0; i < arr.length; i++) {
+  console.log('i love ' + arr[i])
+  }
+}
+
+btn.onclick = function() {
+  console.log('my button is working');
+  console.log(nick.print(nick.movies));
+}
+
+}//end of window onload
