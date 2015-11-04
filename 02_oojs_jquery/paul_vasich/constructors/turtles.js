72b3f00bb98908ddf6b6a6d6e043793894b03e78
Paul Vasich <paul.vasich@gmail.com>
2015-11-04 15:45:10 -0600

    made turtles private w/ closures

--- 02_oojs_jquery/paul_vasich/constructors/turtles.js
+++ 02_oojs_jquery/paul_vasich/constructors/turtles.js
@@ -1,13 +1,16 @@
-function Turtle(name, color, weapon, favPizza) {
-  var _name = name;
-  var _color = color;
-  var _weapon = weapon;
-  var _favPizza = favPizza;
+function Turtle(_name, _color, _weapon, _favPizza) {
 
-  this.getName = function() { return _name; }
-  this.getColor = function() { return _color; }
-  this.getWeapon = function() { return _weapon; }
-  this.getPizza = function() { return _favPizza; }
+  this.getName = function()     { return _name; }
+  this.setName = function(name) { _name = name; }
+
+  this.getColor = function()      { return _color; }
+  this.setColor = function(color) { _color = color; }
+
+  this.getWeapon = function()       { return _weapon; }
+  this.setWeapon = function(weapon) { _weapon = weapon; }
+
+  this.getPizza = function()      { return _favPizza; }
+  this.setPizza = function(pizza) { _pizza = pizza; }
 }
 
 var raphael = new Turtle('Raphael', 'red', 'sai', 'all');
