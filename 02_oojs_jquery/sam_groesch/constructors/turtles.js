fa07cc19b2ae2d70639168558e494735e5e78fb8
Sam Groesch <samgroesch@gmail.com>
2015-11-04 16:17:07 -0600

    Afternoon contructors stuff

---
+++ 02_oojs_jquery/sam_groesch/constructors/turtles.js
@@ -0,0 +1,45 @@
+function turtle(name, color, weapon, favoritePizzaType) {
+  this.name = name;
+  this.color = color;
+  this.weapon = weapon;
+  this.favoritePizzaType = favoritePizzaType;
+}
+
+var donatello = new turtle('Donatello', 'purple', 'bo staff', 'chicken, mushroom, green pepper and tomatoes');
+var raphael = new turtle('Raphael', 'red', 'twin sais', 'pepperoni, Italian sausage, bacon, and beef');
+var michelangelo = new turtle('Michelangelo', 'orange', 'nunchaku', 'pepperoni, ham, pineapple, and jalapeño');
+var leonardo = new turtle('Leonardo', 'blue', 'twin katana swords', 'onions, black olives, green pepper, and mushrooms');
+
+function lego(x, y, z) {
+  this.x = x;
+  this.y = y;
+  this.z = function() {
+    names = z;
+    if (typeof(z) == 'string') {
+      hasName = true;
+    } else {
+      hasName = false;
+    } return [names, hasName];
+  }
+}
+
+
+
+  function() {
+    namer = name;
+    if (name == true) {
+      hasName = true;
+    } else {
+      hasName = false;
+    } return [namer, hasName];
+  }
+}
+
+function tellAboutName(name) {
+  names = name;
+  if (typeof(name) == 'string') {
+    hasName = true;
+  } else {
+    hasName = false;
+  } return [names, hasName];
+}
