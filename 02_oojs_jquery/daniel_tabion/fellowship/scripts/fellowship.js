e9602d93b40557274c5870723b5559a6b19a2bc9
Daniel Tabion <dctabion@gmail.com>
2015-11-05 18:09:02 -0600

    started homework

--- 02_oojs_jquery/daniel_tabion/fellowship/scripts/fellowship.js
+++ 02_oojs_jquery/daniel_tabion/fellowship/scripts/fellowship.js
@@ -19,5 +19,98 @@ var buddies = [
 var lands = ['The Shire', 'Rivendell', 'Mordor'];
 var body = document.body;
 
+// create a section tag with an id of `middle-earth`
+// add each land as an `article` tag
+// inside each `article` tag include an `h1` with the name of the land
+// append `middle-earth` to your document `body`
+//  var section = '<section id='middle-earth'></section>';
+var makeMiddleEarth = function () {
+
+  console.log('makeMiddleEarth');
+  var middleEarth = "<section id='middle-earth'></section>";
+  $(body).append(middleEarth);
+  for (var i in lands) {
+    console.log('adding land: ' + lands[i]);
+    $("#middle-earth").append("<article><h1>" + lands[i] + "</h1></article>");
+  }
+};
+
+makeMiddleEarth();
+
+// display an `unordered list` of hobbits in the shire
+   // (which is the second article tag on the page)
+// give each hobbit a class of `hobbit`
+var makeHobbits = function () {
+    console.log('adding hobbits!!!');
+    console.log($("article")[0]);
+    var theShire = $('article')[0];
+    var list = $(theShire).append("<ul></ul>");
+    for (var i in hobbits) {
+      list.append("<li class='hobbit'>" + hobbits[i] + "</li>");
+      console.log('adding: ', hobbits[i] );
+    }
+};
+
+makeHobbits();
 
 
+var keepItSecretKeepItSafe = function () {
+   // create a div with an id of `'the-ring'`
+   // give the div a class of `'magic-imbued-jewelry'`
+   // add the ring as a child of `Frodo`
+};
+
+var makeBuddies = function () {
+   // create an `aside` tag
+   // attach an `unordered list` of the `'buddies'` in the aside
+   // insert your aside as a child element of `rivendell`
+};
+
+
+var beautifulStranger = function () {
+   // change the `'Strider'` text to `'Aragorn'`
+};
+
+
+var leaveTheShire = function () {
+   // assemble the `hobbits` and move them to `rivendell`
+};
+
+
+var forgeTheFellowShip = function () {
+   // create a new div called `'the-fellowship'` within `rivendell`
+   // add each `hobbit` and `buddy` one at a time to `'the-fellowship'`
+   // after each character is added make an alert that they // have joined your party
+};
+
+
+var theBalrog = function () {
+   // change the `'Gandalf'` text to `'Gandalf the White'`
+   // apply the following style to the element, make the // background 'white', add a grey border
+};
+
+
+var hornOfGondor = function () {
+   // pop up an alert that the horn of gondor has been blown
+   // Boromir's been killed by the Uruk-hai!
+   // Remove `Boromir` from the Fellowship
+};
+
+
+var itsDangerousToGoAlone = function (){
+   // take `Frodo` and `Sam` out of the fellowship and move // them to `Mordor`
+   // add a div with an id of `'mount-doom'` to `Mordor`
+};
+
+
+var weWantsIt = function () {
+  // Create a div with an id of `'gollum'` and add it to Mordor
+  // Remove `the ring` from `Frodo` and give it to `Gollum`
+  // Move Gollum into Mount Doom
+};
+
+
+var thereAndBackAgain = function () {
+   // remove `Gollum` and `the Ring` from the document
+   // Move all the `hobbits` back to `the shire`
+};
