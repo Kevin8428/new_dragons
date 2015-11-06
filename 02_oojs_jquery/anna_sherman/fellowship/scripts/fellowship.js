32cf839ed2148102452d2897dc3611fbfbcc1afa
Anna Sherman <sherman.anna@gmail.com>
2015-11-05 21:36:12 -0600

    oranges and fellowship problems 1 thru 3

--- 02_oojs_jquery/anna_sherman/fellowship/scripts/fellowship.js
+++ 02_oojs_jquery/anna_sherman/fellowship/scripts/fellowship.js
@@ -20,4 +20,33 @@ var lands = ['The Shire', 'Rivendell', 'Mordor'];
 var body = document.body;
 
 
+var makeMiddleEarth = function () {
+  $('body').append('<section id="middle-earth"></section>');
+  for (var land in lands){
+    $('#middle-earth').append('<article><h1>' + lands[land] + '</h1></article>');
+  }
+};
 
+makeMiddleEarth();
+
+
+var makeHobbits = function () {
+      $('article:nth-child(2)').append('<ul></ul>');
+      for (var hobbit in hobbits){
+        $('ul').append('<li class="hobbit">' + hobbits[hobbit] + '</li>');
+      }
+};
+
+makeHobbits();
+
+
+var keepItSecretKeepItSafe = function () {
+  ringDiv = '<div id="the-ring" class="magic-imbued-jewelry"></div>';
+  $('body').append(ringDiv);
+  $(ringDiv).appendTo('li.hobbit:nth-child(1)');
+   // create a div with an id of `'the-ring'`
+   // give the div a class of `'magic-imbued-jewelry'`
+   // add the ring as a child of `Frodo`
+};
+
+keepItSecretKeepItSafe();
