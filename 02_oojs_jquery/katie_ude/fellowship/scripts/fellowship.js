6a26ea22a0f5dfdb7f1001d954a60ed04b08adaa
Katherine Ude <Katie.Ude@gmail.com>
2015-11-05 23:12:33 -0600

    saving LOTR hw progress

--- 02_oojs_jquery/katie_ude/fellowship/scripts/fellowship.js
+++ 02_oojs_jquery/katie_ude/fellowship/scripts/fellowship.js
@@ -1,5 +1,15 @@
-console.log("Linked.");
+$('document').ready(function(){
 
+makeMiddleEarth();
+
+makeHobbits();
+
+keepItSecretKeepItSafe();
+
+makeBuddies();
+
+console.log("Linked.");
+})
 // Dramatis Personae
 var hobbits = [
   'Frodo Baggins',
@@ -19,5 +29,32 @@ var buddies = [
 var lands = ['The Shire', 'Rivendell', 'Mordor'];
 var body = document.body;
 
+var makeMiddleEarth = function() {
+  console.log('I am in middle-earth');
+  $('body').append("<section id='middle-earth'>Middle Earth</section>");
+  for (var i = 0; i < lands.length; i++){
+    console.log('I am still working');
+    var land= "<article><h1>"+lands[i]+"</h1></article>";
+    $('section').append(land);
+  }
+};
 
+var makeHobbits = function() {
+  for (var i = 0; i < hobbits.length; i++) {
+  var hobbitNames = "<ul class='hobbit'>"+ hobbits[i] +"</ul>";
+  $('article h1:first').append(hobbitNames);
+  }
+}
 
+var keepItSecretKeepItSafe = function() {
+  $('.hobbit:first').append("<div id='the-ring' class='magic-imbued-jewelry'></div>");
+}
+
+var makeBuddies = function() {
+  $('article h1:eq(1)').append("<aside></aside>");
+  for (var i = 0; i < buddies.length; i++) {
+    console.log(buddies[i]);
+    var buddyNames = "<ul>" + buddies[i] + "</ul>";
+    $('aside').append(buddyNames);
+  }
+}
