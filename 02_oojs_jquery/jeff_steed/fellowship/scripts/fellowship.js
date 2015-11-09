ec43f6eb672f3142d7beb2267bc4c01437b60b37
Jeff Steed <steed.jeff@gmail.com>
2015-11-08 22:52:41 -0600

    Completed the weekend LOTR assignment.

--- 02_oojs_jquery/jeff_steed/fellowship/scripts/fellowship.js
+++ 02_oojs_jquery/jeff_steed/fellowship/scripts/fellowship.js
@@ -68,7 +68,6 @@ var makeMiddleEarth = function() {
   //**************************************************************************//
 };
 
-makeMiddleEarth();
 
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
 //                                Part 2                                      \\
@@ -102,7 +101,6 @@ var makeHobbits = function () {
 
 };
 
-makeHobbits();
 
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
 //                                  Part 3                                    \\
@@ -126,7 +124,6 @@ var keepItSecretKeepItSafe = function () {
   //**************************************************************************//
 };
 
-keepItSecretKeepItSafe();
 
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
 //                                Part 4                                      \\
@@ -172,7 +169,6 @@ var makeBuddies = function () {
   //**************************************************************************//
 };
 
-makeBuddies();
 
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
 //                                 Part 5                                     \\
@@ -183,15 +179,14 @@ var beautifulStranger = function () {
   // Using jQuery
   $('#middle-earth article:nth-child(2) ul:first-child li:nth-child(4)').html('Aragorn')
 
-  //**************************************************************************\\
-  // // Using vanilla JavaScript                                              \\
-  // var middleEarth = document.getElementById('middle-earth');               \\
-  // var buds = middleEarth.firstChild.nextSibling.lastChild.firstChild;      \\
-  // buds.getElementsByTagName('li')[3].innerHTML = 'Aragorn';                \\
-  //**************************************************************************\\
+  //**************************************************************************//
+  // // Using vanilla JavaScript                                              //
+  // var middleEarth = document.getElementById('middle-earth');               //
+  // var buds = middleEarth.firstChild.nextSibling.lastChild.firstChild;      //
+  // buds.getElementsByTagName('li')[3].innerHTML = 'Aragorn';                //
+  //**************************************************************************//
 };
 
-beautifulStranger();
 
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
 //                                   Part 6                                   \\
@@ -203,22 +198,249 @@ var leaveTheShire = function () {
   var $hobbits = $('#middle-earth article:first-child ul');
   $('#middle-earth article:nth-child(2)').append($hobbits);
 
-  //**************************************************************************\\
-  // // Using vanilla JavaScript                                              \\
-  // var middleEarth = document.getElementById('middle-earth');               \\
-  // var rivendell = middleEarth.firstChild.nextSibling;                      \\
-  // var daHobbits = middleEarth.firstChild.lastChild;                        \\
-  // rivendell.appendChild(daHobbits);                                        \\
-  //**************************************************************************\\
+  //**************************************************************************//
+  // // Using vanilla JavaScript                                              //
+  // var middleEarth = document.getElementById('middle-earth');               //
+  // var rivendell = middleEarth.firstChild.nextSibling;                      //
+  // var daHobbits = middleEarth.firstChild.lastChild;                        //
+  // rivendell.appendChild(daHobbits);                                        //
+  //**************************************************************************//
 };
 
-leaveTheShire();
 
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
 //                                    Part 7                                  \\
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
 var forgeTheFellowShip = function () {
   // create a new div called `'the-fellowship'` within `rivendell`
+
+  // Using jQuery
+  var $fellows = $('<div>', {name: 'the-fellowship'});
+  $('#middle-earth article:nth-child(2)').append($fellows);
+
+  //**************************************************************************//
+  // // Using vanilla JavaScript                                              //
+  // var middleEarth = document.getElementById('middle-earth');               //
+  // var daFellows = document.createElement('div');                           //
+  // daFellows.setAttribute('name', 'the-fellowship');                        //
+  // var rivendell = middleEarth.firstChild.nextSibling;                      //
+  // rivendell.appendChild(daFellows);                                        //
+  //**************************************************************************//
+
   // add each `hobbit` and `buddy` one at a time to `'the-fellowship'`
   // after each character is added make an alert that they // have joined your party
+
+  // Using jQuery
+  var $ul = $('<ul>');
+  for (var hobbit in hobbits) {
+    var $li = $('<li>', {class: 'hobbit'});
+    if (hobbits[hobbit] == 'Frodo Baggins') {
+      $li.html(hobbits[hobbit]);
+      var $div = $('<div>', {id: 'the-ring', class: 'magic-imbued-jewelry'});
+      $li.append($div);
+    } else {
+      $li.html(hobbits[hobbit]);
+    }
+    alert($li.text() + ' has joined the fellowship.');
+    $ul.append($li);
+  }
+  for (var bud in buddies) {
+    var $li = $('<li>');
+    $li.html(buddies[bud]);
+    if (buddies[bud] == 'Strider') {
+      $li.html('Aragorn');
+    }
+    alert($li.text() + ' has joined the fellowship.');
+    $ul.append($li);
+  }
+  $fellows.append($ul);
+
+  $('#middle-earth article:nth-child(2) ul')[1].remove();
+  $('#middle-earth article:nth-child(2) aside').remove();
+
+  //********************************************************************************************//
+  // // Using vanilla JavaScript                                                                //
+  // var newList = document.createElement('ul');                                                //
+  // var daHobbits = document.getElementsByClassName('hobbit');                                 //
+  // while (daHobbits.length > 0) {                                                             //
+  //   newList.appendChild(daHobbits[0]);                                                       //
+  //   alert(newList.lastChild.innerText + " has joined the Fellowship.");                      //
+  // }                                                                                          //
+  // var buds = rivendell.firstChild.nextSibling.firstChild.getElementsByTagName('li');         //
+  // while (buds.length > 0) {                                                                  //
+  //   newList.appendChild(buds[0]);                                                            //
+  //   alert(newList.lastChild.innerText + " has joined the Fellowship.");                      //
+  // }                                                                                          //
+  // daFellows.appendChild(newList);                                                            //
+  // rivendell.removeChild(rivendell.firstChild.nextSibling.nextSibling);                       //
+  // rivendell.removeChild(rivendell.firstChild.nextSibling);                                   //
+  //********************************************************************************************//
 };
+
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
+//                                 Part 8                                     \\
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
+var theBalrog = function () {
+  // change the `'Gandalf'` text to `'Gandalf the White'`
+
+  // Using jQuery
+  var $gandalf = $('#middle-earth article:nth-child(2) div ul li:nth-child(5)');
+  $gandalf.html('Gandalf the White');
+
+  //********************************************************************************************************//
+  // // Using vanilla JavaScript                                                                            //
+  // var middleEarth = document.getElementById('middle-earth');                                             //
+  // middleEarth.firstChild.nextSibling.lastChild.firstChild.childNodes[4].innerHTML = 'Gandalf the White'; //
+  //********************************************************************************************************//
+
+  // apply the following style to the element, make the // background 'white', add a grey border
+
+  // Using jQuery
+  $gandalf.css({background: 'white', border: '4px solid grey'});
+
+  //********************************************************************************************************//
+  // // Using vanilla JavaScript                                                                            //
+  // middleEarth.firstChild.nextSibling.lastChild.firstChild.childNodes[4].style.backgroundColor = 'white'; //
+  // middleEarth.firstChild.nextSibling.lastChild.firstChild.childNodes[4].style.border = '4px solid grey'; //
+  //********************************************************************************************************//
+};
+
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
+//                                   Part 9                                   \\
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
+var hornOfGondor = function () {
+  // pop up an alert that the horn of gondor has been blown
+  // Boromir's been killed by the Uruk-hai!
+  alert('The Horn of Gondor has been blown! Boromir has been killed by the Uruk-hai!');
+  // Remove `Boromir` from the Fellowship
+
+  // Using jQuery
+  $('#middle-earth article:nth-child(2) div ul li:nth-child(9)').remove();
+
+  //**********************************************************************************//
+  // // Using vanilla JavaScript                                                      //
+  // var middleEarth = document.getElementById('middle-earth');                       //
+  // var fellows = middleEarth.firstChild.nextSibling.lastChild.firstChild;           //
+  // fellows.removeChild(fellows.childNodes[8]);                                      //
+  //**********************************************************************************//
+};
+
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
+//                                    Part 10                                 \\
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
+var itsDangerousToGoAlone = function (){
+  // take `Frodo` and `Sam` out of the fellowship and move // them to `Mordor`
+
+  // Using jQuery
+  var $ul = $('<ul>');
+  var $frodo = $('#middle-earth article:nth-child(2) div ul li:nth-child(1)').remove();
+  $ul.append($frodo);
+  var $sam = $('#middle-earth article:nth-child(2) div ul li:nth-child(1)').remove();
+  $ul.append($sam);
+  $('#middle-earth article:nth-child(3)').append($ul);
+
+  //****************************************************************//
+  // // Using vanilla JavaScript                                    //
+  // var middleEarth = document.getElementById('middle-earth');     //
+  // var rivendell = middleEarth.firstChild.nextSibling;            //
+  // var ul = document.createElement('ul');                         //
+  // ul.appendChild(rivendell.lastChild.firstChild.childNodes[0]);  //
+  // ul.appendChild(rivendell.lastChild.firstChild.childNodes[0]);  //
+  // rivendell.nextSibling.appendChild(ul);                         //
+  //****************************************************************//
+
+  // add a div with an id of `'mount-doom'` to `Mordor`
+
+  // Using jQuery
+  var $div = $('<div>', {id: 'mount-doom'});
+  $('#middle-earth article:nth-child(3)').append($div);
+
+  //********************************************//
+  // // Using vanilla JavaScript                //
+  // var div = document.createElement('div');   //
+  // div.setAttribute('id', 'mount-doom');      //
+  // rivendell.nextSibling.appendChild(div);    //
+  //********************************************//
+};
+
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
+//                                    Part 11                                 \\
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
+var weWantsIt = function () {
+  // Create a div with an id of `'gollum'` and add it to Mordor
+
+  // Using jQuery
+  var $gollum = $('<div>', {id: 'gollum'});
+  $('#middle-earth article:nth-child(3)').append($gollum);
+
+  //************************************************************//
+  // // Using vanilla JavaScript                                //
+  // var gollum = document.createElement('div');                //
+  // gollum.setAttribute('id', 'gollum');                       //
+  // var middleEarth = document.getElementById('middle-earth'); //
+  // middleEarth.lastChild.appendChild(gollum);                 //
+  //************************************************************//
+
+  // Remove `the ring` from `Frodo` and give it to `Gollum`
+  // Move Gollum into Mount Doom
+
+  // Using jQuery
+  $gollum.append($('#the-ring'));
+  $('#mount-doom').append($gollum);
+
+  //******************************************************//
+  // // Using vanilla JavaScript                          //
+  // var daRing = document.getElementById('the-ring');    //
+  // gollum.appendChild(daRing);                          //
+  // var mtDoom = document.getElementById('mount-doom');  //
+  // mtDoom.appendChild(gollum);                          //
+  //******************************************************//
+};
+
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
+//                                   Part 12                                  \\
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
+var thereAndBackAgain = function () {
+  // remove `Gollum` and `the Ring` from the document
+
+  // Using jQuery
+  $('#gollum').remove();
+
+  //****************************************************************************************//
+  // // Using vanilla JavaScript                                                            //
+  // document.getElementById('mount-doom').removeChild(document.getElementById('gollum'));  //
+  //****************************************************************************************//
+
+  // Move all the `hobbits` back to `the shire`
+
+  // Using jQuery
+  var $hobbits = $('.hobbit').remove();
+  $('#middle-earth article:first-child').append($hobbits);
+
+  //**************************************************************************************************************************//
+  // // Using vanilla JavaScript                                                                                              //
+  // var middleEarth = document.getElementById('middle-earth');                                                               //
+  // middleEarth.firstChild.appendChild(middleEarth.lastChild.firstChild.nextSibling.childNodes[0]);                          //
+  // middleEarth.firstChild.appendChild(middleEarth.lastChild.firstChild.nextSibling.childNodes[0]);                          //
+  // middleEarth.firstChild.appendChild(middleEarth.firstChild.nextSibling.firstChild.nextSibling.firstChild.childNodes[0]);  //
+  // middleEarth.firstChild.appendChild(middleEarth.firstChild.nextSibling.firstChild.nextSibling.firstChild.childNodes[0]);  //
+  //**************************************************************************************************************************//
+};
+
+makeMiddleEarth();
+makeHobbits();
+keepItSecretKeepItSafe();
+makeBuddies();
+beautifulStranger();
+leaveTheShire();
+forgeTheFellowShip();
+theBalrog();
+hornOfGondor();
+itsDangerousToGoAlone();
+weWantsIt();
+thereAndBackAgain();
