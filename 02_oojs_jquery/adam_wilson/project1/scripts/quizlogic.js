4179764bd82982c802b9b7d0b93b4748b249448d
Adam Wilson <adamwilson1234@gmail.com>
2015-11-11 11:34:49 -0600

    project 1 says welcome to state array element name array elem lives here and go button dispalys a photo on click. click brings next message iterateing thru array. i'm building code to add the map in scratch_paper.js separate file.

--- 02_oojs_jquery/adam_wilson/project1/scripts/quizlogic.js
+++ 02_oojs_jquery/adam_wilson/project1/scripts/quizlogic.js
@@ -6,40 +6,141 @@
 //1. welcome to________ (data)
 
 // put divs in variables
+var ui = {}; // object to hold dom variables
 var quizArea = $('.quizArea');
-var quizQuestion = $('.quizQuestion');
+var quizBackground = $('.quizBackground');
 
 // define variables to call
-var names = ['Kathy', 'Howard', 'Renee', 'Adam'];
+var names = ['Grandma Kathy', 'Grandpa Howard', 'Aunt Renee', 'Uncle Adam'];
+var states = ['Ohio', 'Ohio', 'Illinois', 'Illinois'];
+var images = ['<img src="images/family_photo.jpg" />', '<img src="images/family_photo.jpg" />', '<img src="images/family_photo.jpg" />', '<img src="images/family_photo.jpg" />'];
+var inc = 0;
+var img = $('img')
+var imageLocation = $('#imageLocation');
+
+
+
+
+
+ /////////////////////////////////////////////
+ // doc ready : inside is where action happens
+ $(document).ready(function() {
+   //  img.hide();
+
+
+
+
+
+
+
+/////////button action calls function/////////
+    $('button').click( function() {
+
+      $("p").remove();
+
+      if (inc < names.length) {
+         quizBackground.append('<p>Welcome to ' + states[inc] + '.</p>');
+        quizBackground.append('<p>' + names[inc] + ' lives in ' + states[inc] + '.</p>');
+
+        //display images using inc to select in array
+        imageLocation.append(images[inc]);
+
+        // put markers on map (put map above.....)
+        // gmpaps create an array of
+        // set lat, long for api
+
+
+
+
+
+
+
+
+
+
+
+        inc += 1; // increment at end of loop
+     } else {
+        quizBackground.append('The Family is All Here!');
+     }
+
+     }) // end button click function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // display quiz background div
+    // quizArea.append(quizBackground);
+    //display question
+    // showQuestion();
+    // //display question text
+    //    //type _____'s name
+    // for (var i = 0; i < names.length; i++) {
+    //       quizBackground.append('<p>Welcome. Type the name of ' + names[i] + '.</p>');
+    //    }
+    // //display next question button
+    //
+    // for (var i = 0; i < names.length; i++) {
+    //    quizBackground.append('<p>Welcome. Type the name of ' + names[i] + '.</p>');
+    // }
+ }); // end of doc.ready
+ ////////////////////////////////////////
+
+
+
+
 // var questionSentence = 'Type ' + names[i] + '\'s name and press GO.';
+// inc += 1 in order to update question name
+/*function incrementName() {*/
+  /*inc += 1;
+  var questionText = '<p>Type the name ' + names[inc] + '.</p>';*/
+/*};*/
+
+// display a question and button
+/*$('.quizBackground').append(questionText);
+$('.quizBackground').append('<button>Go</button>');*/
+
+
+
+
+  /*var questionText = '<p>Type the name ' + names[inc] + '.</p>';
+  $('.quizBackground').append(questionText);
+  inc += 1;*/
+
+
+
+
+
+
+
+
 
 // define functions to call
 // function showQuestion(names) {
-//    quizQuestion.append(questionSentence)
+//    quizBackground.append(questionSentence)
 // };
-////////////////////////////////////////
-// doc ready : inside is where action happens
-$(document).ready(function() {
-   // display quiz background div
-   // quizArea.append(quizQuestion);
 
-   //display question
-   // showQuestion();
-   // //display question text
-   //    //type _____'s name
-   // for (var i = 0; i < names.length; i++) {
-   //       quizQuestion.append('<p>Welcome. Type the name of ' + names[i] + '.</p>');
-   //    }
-   // //display next question button
-   //
-   // for (var i = 0; i < names.length; i++) {
-   //    quizQuestion.append('<p>Welcome. Type the name of ' + names[i] + '.</p>');
-   // }
-
-
-
-}); // end of doc.ready
-////////////////////////////////////////
 
 
 //2. message who lives here? hint: enter grandma's name
