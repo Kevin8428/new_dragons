9018b2a538df130e954d1b0de5a823d95b44d061
Jason Tham <jtham@sbcglobal.net>
2015-11-17 17:08:38 -0600

    finished rups, start hw

---
+++ 05_ruby101/jason_tham/ruby_intro/loop_cheatsheet.rb
@@ -0,0 +1,23 @@
+num = 1
+#
+# while num < 11
+#   p num
+#   num = num + 1
+# end
+#
+# #
+# until num > 10
+#   p num
+#   num +=1
+# end
+# for num in 0..5
+#   if num > 2
+#     p "greater than true"
+#   end
+#   p num
+# end
+
+# animals = ["dogs", "wolf"]
+# for animal in animals
+#   p animal
+# end
