46f52eae72fc9ec0ee0c56faad60e01c265e2a78
Paul Boyle <pboyle17@gmail.com>
2015-11-17 16:37:17 -0600

    homework and rups finished

--- 05_ruby101/paul_boyle/hmwk1/hmwk1.rb
+++ 05_ruby101/paul_boyle/hmwk1/hmwk1.rb
@@ -1,127 +1,193 @@
 words = ['hello','what','is','up','dude']
-# def lengths (arr)
-#   len=[]
-#   for word in arr
-#     len.push(word.length)
-#   end
-#   p len
-# end
+def lengths (arr)
+  len=[]
+  for word in arr
+    len.push(word.length)
+  end
+  p len
+end
 
-# def lengths (arr)
-#   return arr.map!{|word| word.length}
-# end
-#
-# lengths(words)
-#
-# def trans(num1,num2,num3)
-#   return (num1*num2)**num3
-# end
-#
-# p trans(1,3,3)#8
-#
-# def toonify (accent,sentence)
-#   if accent.downcase == 'daffy'
-#     return  p sentence.gsub 's', 'th'
-#   else
-#     return p sentence.gsub 'r','w'
-#   end
-# end
-# toonify("daffy", "so you smell like sausage")
-#
-# def letterReverse(str)
-#   return str.split(' ').each{|word| word.reverse!}.join(' ')
-# end
-#
-# p letterReverse("Put Hans back on the line")
-#
-# def wordReverse(str)
-#   return str.split(' ').reverse.join(' ')
-# end
-#
-# p wordReverse('now i know what tv dinner feels like')
-#
-# def longest (arr)
-#   return arr.max{|a,b| a.length <=> b.length}
-# end
-#
-# p longest(["Nothing" , "takes", "the", "taste", "out", "of", "peanut", "butter", "quite", "like", "unrequited", "love"])
+def lengths (arr)
+  return arr.map!{|word| word.length}
+end
 
+lengths(words)
 
-#######################################################################################################################
-#######################################################################################################################
-#######################################################################################################################
-#######################################################################################################################
-#######################################################################################################################
+def trans(num1,num2,num3)
+  return (num1*num2)**num3
+end
+
+p trans(1,3,3)#8
+
+def toonify (accent,sentence)
+  if accent.downcase == 'daffy'
+    return  p sentence.gsub 's', 'th'
+  else
+    return p sentence.gsub 'r','w'
+  end
+end
+toonify("daffy", "so you smell like sausage")
+
+def letterReverse(str)
+  return str.split(' ').each{|word| word.reverse!}.join(' ')
+end
+
+p letterReverse("Put Hans back on the line")
+
+def wordReverse(str)
+  return str.split(' ').reverse.join(' ')
+end
+
+p wordReverse('now i know what tv dinner feels like')
+
+def longest (arr)
+  return arr.max{|a,b| a.length <=> b.length}
+end
+
+p longest(["Nothing" , "takes", "the", "taste", "out", "of", "peanut", "butter", "quite", "like", "unrequited", "love"])
 
 
-# def greaterThanTwo(arr)
-#   arr.each do |thing|
+# Round 1: Prime Time
 #
-#     end
-#   end
+# Write a method called prime? that takes a single parameter called number and returns true if the parameter is a prime number, or false otherwise.
+# Use the Math.sqrt ...
 
-
-def greaterThanTwo(arr)
-  arr.each do |num|
-    if num > 2
-      p num
-    else
-      p 'not greater than two'
+def prime? (number)
+  if number == 2
+    return true
+  elsif number < 2
+    return false
+  else
+    for num in 2..Math.sqrt(number)
+      if number % num == 0
+        return false
+      else
+        return true
+      end
     end
   end
 end
 
-p greaterThanTwo([2,3,4,5])
+p prime?(17)
+p prime?(2)
+p prime?(10000001)
 
-p adriana = {name: 'adriana',
-  :age =>23}
-p james = Hash.new
+# Round 2: Fardingworth Falls
+#
+# Let's generate some random town names for a Tycoon-style video-game. We can do this by combining the following generic name fragments:
+# Starts: Bed, Brunn, Dun, Far, Glen, Tarn
+# Middles: ding, fing, ly, ston
+# Ends: borough, burg, ditch, hall, pool, ville, way, worth
+# Step 1
+#
+# Write a method called town_names that randomly generates a number of town names by combining one Start, one Middle, and one End. Calling town_names(5) should give an array of 5 town names. If just town_names is called, generate 3 names.
+# Step 2
+#
+# Modify the method so that calling town_names(3, 'near_water') will randomly add either "-on-sea" or " Falls" to each of the names. Optionally, think of another value that the second argument could have, and add appropriate random suffixes or prefixes when it is provided.
+# Step 3
+#
+# Modify the method so that calling town_names(3, 'short_name') will always generate names without a Middle.
 
-paul = {:name=> 'Paul',
-        :eyeColor => 'blue'}
-p paul
+def town_names(amount)
+  starts=['Bed','Brunn','Dun','Far','Glen','Tarn']
+  middles=['ding','fing','ly','ston']
+  ends=['borough','burg','ditch','hall','pool','ville','way','worth']
+  names=[];
+
+  amount.times do
+    names.push(starts.sample+middles.sample+ends.sample)
+  end
+
+  return names
+end
+
+p town_names(10)
 
 
 
 
+
+
+
+
+
+
+#
+# #######################################################################################################################
+# #######################################################################################################################
+# #######################################################################################################################
+# #######################################################################################################################
+# #######################################################################################################################
+#
+#
+# # def greaterThanTwo(arr)
+# #   arr.each do |thing|
+# #
+# #     end
+# #   end
+#
+#
+# def greaterThanTwo(arr)
+#   arr.each do |num|
+#     if num > 2
+#       p num
+#     else
+#       p 'not greater than two'
+#     end
+#   end
+# end
+#
+# p greaterThanTwo([2,3,4,5])
+#
+# p adriana = {name: 'adriana',
+#   :age =>23}
+# p james = Hash.new
+#
+# paul = {:name=> 'Paul',
+#         :eyeColor => 'blue'}
+# p paul
+#
+#
 #
 #
 # #
+# #
+# # #
+# # # # # #
+# # # # # # words.each do |x|
+# # # # # #    p words[x].length
+# # # # # #  end
 # # # # #
-# # # # # words.each do |x|
-# # # # #    p words[x].length
+# # # # #
+# # # # #  num = 1
+# # # # #  while num < 11
+# # # # #    p num
+# # # # #    num+=1
 # # # # #  end
-# # # #
-# # # #
-# # # #  num = 1
-# # # #  while num < 11
-# # # #    p num
-# # # #    num+=1
-# # # #  end
-# # # #
-# # # #
-# # # num = 0
-# # # until num > 10
-# # #   p num
-# # #   num +=1
-# # # end
-# # # num = 0
-# # # for num in 0..5
-# # #   p num
-# # #   num +=1
-# # # end
-# #
-# # # for num in 0..5
-# # #   if num > 2
-# # #     p 'greater than two'
-# # #   end
-# # #   p num
-# # # end
-# # # animals = ['cats','dogs','sup']
-# # # for a in animals
-# #   # p a
+# # # # #
+# # # # #
+# # # # num = 0
+# # # # until num > 10
+# # # #   p num
+# # # #   num +=1
 # # # # end
-# #
-# # teachers = ['jim','adriana','james']
-# #  p teachers.length
-# #  p teachers.count{|name| name == 'adriana'}
+# # # # num = 0
+# # # # for num in 0..5
+# # # #   p num
+# # # #   num +=1
+# # # # end
+# # #
+# # # # for num in 0..5
+# # # #   if num > 2
+# # # #     p 'greater than two'
+# # # #   end
+# # # #   p num
+# # # # end
+# # # # animals = ['cats','dogs','sup']
+# # # # for a in animals
+# # #   # p a
+# # # # # end
+# # #
+# # # teachers = ['jim','adriana','james']
+# # #  p teachers.length
+# # #  p teachers.count{|name| name == 'adriana'}
